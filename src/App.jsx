c6a2ebae950753a923409fba381fcdb989e9acc7
aeonshad <shadaeon@gmail.com>
2024-10-01 16:28:22 +0200

    🔧 Update: Implemented className prop for Modal and other sub-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import { useState } from 'react';
-import { Modal, ModalContent, ModalDescription, ModalFooter, ModalHeader, ModalTitle } from './pkg/Modal/Modal';
+import { Modal, ModalContent, ModalClose, ModalDescription, ModalFooter, ModalHeader, ModalTitle } from './pkg/Modal/Modal';
 
 function App() {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
         <main>
-            <button onClick={() => setIsOpen(true)}>Open Modal</button>
-            <Modal open={isOpen}>
-                <ModalContent onClose={() => setIsOpen(false)}>
-                    <ModalHeader>
-                        <ModalTitle>Title</ModalTitle>
-                        <ModalDescription>Description</ModalDescription>
+            <button className="btn btn-primary" onClick={() => setIsOpen(true)}>
+                Open Modal
+            </button>
+            <Modal className="modal-overlay" open={isOpen}>
+                <ModalContent className="modal-container">
+                    <ModalClose className="modal-icon" onClose={() => setIsOpen(false)} />
+                    <img className="modal-hero" src="./hero.svg" alt="hero" />
+                    <ModalHeader className="modal-header">
+                        <ModalTitle className="modal-title">Welcome to React Ease Modal</ModalTitle>
+                        <ModalDescription className="modal-description">You are ready to use my component!</ModalDescription>
                     </ModalHeader>
-                    <ModalFooter>
-                        <button onClick={() => setIsOpen(false)}>Close Modal</button>
+                    <ModalFooter className="modal-footer">
+                        <button className="btn btn-secondary" onClick={() => setIsOpen(false)}>
+                            Close Modal
+                        </button>
                     </ModalFooter>
                 </ModalContent>
             </Modal>
--- src/pkg/Modal/Modal.jsx
+++ src/pkg/Modal/Modal.jsx
@@ -1,12 +1,15 @@
 import { ReactComponent as CloseIcon } from './x.svg';
-export const Modal = ({ open, children }) => (open ? <div>{children}</div> : null);
-export const ModalContent = ({ onClose, children }) => (
-    <div>
-        {children}
-        <CloseIcon onClick={onClose} />
-    </div>
-);
-export const ModalHeader = ({ children }) => <div>{children}</div>;
-export const ModalTitle = ({ children }) => <h2>{children}</h2>;
-export const ModalDescription = ({ children }) => <p>{children}</p>;
-export const ModalFooter = ({ children }) => <div>{children}</div>;
+
+export const Modal = ({ open, children, className }) => (open ? <div className={className}>{children}</div> : null);
+
+export const ModalContent = ({ children, className }) => <div className={className}>{children}</div>;
+
+export const ModalClose = ({ onClose, className }) => <CloseIcon className={className} onClick={onClose} />;
+
+export const ModalHeader = ({ children, className }) => <div className={className}>{children}</div>;
+
+export const ModalTitle = ({ children, className }) => <h2 className={className}>{children}</h2>;
+
+export const ModalDescription = ({ children, className }) => <p className={className}>{children}</p>;
+
+export const ModalFooter = ({ children, className }) => <div className={className}>{children}</div>;
